da806cb957367edd1bb9bba9376af0a803fda76b
jedyobidan <jedyobidan@gmail.com>
2013-11-21 20:10:13 -0600

    Simplification of Trigger System

--- src/net/fe/Trigger.java
+++ src/net/fe/Trigger.java
@@ -1,5 +1,21 @@
 package net.fe;
 
-public abstract class Trigger {
-	
+public abstract class Trigger<E> {
+	private int chance;
+	protected boolean success;
+	public Trigger(int chance){
+		this.chance = chance;
+	}
+	public Trigger(){
+		this(100);
+	}
+	public void clear(){
+		success = false;
+	}
+	public void attempt(){
+		if(RNG.get() < chance){
+			success = true;
+		}
+	}
+	public abstract E run(Object[] args);
 }
