0afae4dae0af99ac43957fa47b401081b90f34ae
jedyobidan <jedyobidan@gmail.com>
2013-11-25 20:32:24 -0600

    Oops forgot this.

---
+++ src/net/fe/overworldStage/TerrainTrigger.java
@@ -0,0 +1,11 @@
+package net.fe.overworldStage;
+
+public abstract class TerrainTrigger {
+	public void startOfTurn(Grid g, int x, int y){
+		
+	}
+	
+	public void endOfTurn(Grid g, int x, int y){
+		
+	}
+}
