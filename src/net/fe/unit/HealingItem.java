48d9649adda3a2b73e02cf50f64400ee49ba1a05
jedyobidan <jedyobidan@gmail.com>
2013-12-12 20:16:43 -0600

    Healing Items icons

--- src/net/fe/unit/HealingItem.java
+++ src/net/fe/unit/HealingItem.java
@@ -2,15 +2,16 @@ package net.fe.unit;
 
 
 public class HealingItem extends Item {
-	public static HealingItem VULNERARY = new HealingItem("Vulnerary", 10);
-	public static HealingItem CONCOCTION = new HealingItem("Concoction", 20);
-	public static HealingItem ELIXIR = new HealingItem("Elixir", 99);
+	public static HealingItem VULNERARY = new HealingItem("Vulnerary", 10, 67);
+	public static HealingItem CONCOCTION = new HealingItem("Concoction", 20, 68);
+	public static HealingItem ELIXIR = new HealingItem("Elixir", 99, 69);
 	
 	public int amount;
-	public HealingItem(String name, int amount){
+	public HealingItem(String name, int amount, int id){
 		super(name);
 		setMaxUses(3);
 		this.amount = amount;
+		this.id = id;
 	}
 	public int use(Unit user){
 		super.use(user);
