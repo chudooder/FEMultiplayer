dac39db9904903426f4334f3d07d18cacd9a6ca4
jedyobidan <jedyobidan@gmail.com>
2013-12-13 17:55:01 -0600

    Minor fixes

--- src/net/fe/overworldStage/BattlePreview.java
+++ src/net/fe/overworldStage/BattlePreview.java
@@ -23,19 +23,19 @@ public class BattlePreview extends Entity {
 		defender = d;
 		sprites = new ArrayList<Sprite>();
 		rightArrow = new Sprite();
-		rightArrow.addAnimation("default", Resources.getTexture("gui_selectArrow"),
-				8, 8, 6, 6, 100);
+		rightArrow.addAnimation("default",
+				Resources.getTexture("gui_selectArrow"), 8, 8, 6, 6, 100);
 		leftArrow = new Sprite();
-		leftArrow.addAnimation("default", Resources.getTexture("gui_selectArrow"),
-				8, 8, 6, 6, 100);
+		leftArrow.addAnimation("default",
+				Resources.getTexture("gui_selectArrow"), 8, 8, 6, 6, 100);
 		sprites.add(rightArrow);
 		sprites.add(leftArrow);
 		this.range = range;
 		this.renderDepth = OverworldStage.MENU_DEPTH;
 	}
-	
+
 	public void onStep() {
-		for(Sprite s : sprites) {
+		for (Sprite s : sprites) {
 			s.update();
 		}
 	}
@@ -46,60 +46,80 @@ public class BattlePreview extends Entity {
 		String dHp = String.format("%d", defender.getHp());
 		Transform flip = new Transform();
 		flip.flipHorizontal();
-		
+
 		if (attacker.getWeapon().range.contains(range)) {
-			aHit = String.format("%d", Math.max(0, Math.min(100, 
-					attacker.hit() - defender.avoid())));
+			aHit = String.format(
+					"%d",
+					Math.max(0,
+							Math.min(100, attacker.hit() - defender.avoid())));
 			aAtk = String.format("%d",
 					CombatCalculator.calculateBaseDamage(attacker, defender));
-			aCrit = String.format("%d", Math.max(0, Math.min(100, 
-					attacker.crit() - defender.avoid())));
+			aCrit = String.format(
+					"%d",
+					Math.max(0,
+							Math.min(100, attacker.crit() - defender.avoid())));
 		}
 		if (defender.getWeapon().range.contains(range)) {
-			dHit = String.format("%d", Math.max(0, Math.min(100, 
-					defender.hit() - attacker.avoid())));
+			dHit = String.format(
+					"%d",
+					Math.max(0,
+							Math.min(100, defender.hit() - attacker.avoid())));
 			dAtk = String.format("%d",
 					CombatCalculator.calculateBaseDamage(defender, attacker));
-			dCrit = String.format("%d", Math.max(0, Math.min(100, 
-					defender.crit() - attacker.avoid())));
+			dCrit = String.format(
+					"%d",
+					Math.max(0,
+							Math.min(100, defender.crit() - attacker.avoid())));
 		}
 
-		// Top item and name
+		// Borders
+		Renderer.drawRectangle(x - 1, y - 1, x + 91, y + 132, renderDepth,
+				FightStage.BORDER_DARK);
 		Renderer.drawRectangle(x, y, x + 90, y + 35, renderDepth,
+				FightStage.BORDER_LIGHT);
+		Renderer.drawRectangle(x + 59, y + 35, x + 90, y + 96, renderDepth,
+				FightStage.BORDER_LIGHT);
+		Renderer.drawRectangle(x, y + 35, x + 31, y + 96, renderDepth,
+				FightStage.BORDER_LIGHT);
+		Renderer.drawRectangle(x, y + 96, x + 90, y + 131, renderDepth,
+				FightStage.BORDER_LIGHT);
+
+		// Top item and name
+		Renderer.drawRectangle(x + 1, y + 1, x + 89, y + 34, renderDepth,
 				attacker.getPartyColor());
 		Renderer.drawString("default_med", attacker.name, x + 2, y + 2,
 				renderDepth);
 		new ItemDisplay((int) x + 7, (int) y + 16, attacker.getWeapon())
 				.render();
-		leftArrow.render(x, y+21, renderDepth);
-		rightArrow.renderTransformed(x+90, y+21, renderDepth, flip);
-		
+		leftArrow.render(x, y + 21, renderDepth);
+		rightArrow.renderTransformed(x + 90, y + 21, renderDepth, flip);
 
 		// Attacker stats
-		Renderer.drawRectangle(x + 60, y + 35, x + 90, y + 96, renderDepth,
+		Renderer.drawRectangle(x + 60, y + 34, x + 89, y + 96, renderDepth,
 				attacker.getPartyColor());
-		Renderer.drawString("default_med", aHp, x + 62, y + 36, renderDepth);
-		Renderer.drawString("default_med", aAtk, x + 62, y + 51, renderDepth);
-		Renderer.drawString("default_med", aHit, x + 62, y + 66, renderDepth);
-		Renderer.drawString("default_med", aCrit, x + 62, y + 81, renderDepth);
-		
+		Renderer.drawString("default_med", aHp, x + 63, y + 36, renderDepth);
+		Renderer.drawString("default_med", aAtk, x + 63, y + 51, renderDepth);
+		Renderer.drawString("default_med", aHit, x + 63, y + 66, renderDepth);
+		Renderer.drawString("default_med", aCrit, x + 63, y + 81, renderDepth);
+
 		// Stats text
-		Renderer.drawRectangle(x+30, y+35, x+60, y+96, renderDepth, FightStage.NEUTRAL);
-		Renderer.drawString("default_med", "HP", x+37, y+ 36, renderDepth);
-		Renderer.drawString("default_med", "Atk", x+36, y+ 51, renderDepth);
-		Renderer.drawString("default_med", "Hit", x+36, y+ 66, renderDepth);
-		Renderer.drawString("default_med", "Crt", x+36, y+ 81, renderDepth);
-		
+		Renderer.drawRectangle(x + 31, y + 35, x + 59, y + 96, renderDepth,
+				FightStage.NEUTRAL);
+		Renderer.drawString("default_med", "HP", x + 37, y + 36, renderDepth);
+		Renderer.drawString("default_med", "Atk", x + 36, y + 51, renderDepth);
+		Renderer.drawString("default_med", "Hit", x + 36, y + 66, renderDepth);
+		Renderer.drawString("default_med", "Crt", x + 36, y + 81, renderDepth);
+
 		// Defender stats
-		Renderer.drawRectangle(x, y + 35, x + 30, y + 96, renderDepth,
+		Renderer.drawRectangle(x + 1, y + 35, x + 30, y + 97, renderDepth,
 				defender.getPartyColor());
-		Renderer.drawString("default_med", dHp, x + 2, y + 36, renderDepth);
-		Renderer.drawString("default_med", dAtk, x + 2, y + 51, renderDepth);
-		Renderer.drawString("default_med", dHit, x + 2, y + 66, renderDepth);
-		Renderer.drawString("default_med", dCrit, x + 2, y + 81, renderDepth);
-		
+		Renderer.drawString("default_med", dHp, x + 4, y + 36, renderDepth);
+		Renderer.drawString("default_med", dAtk, x + 4, y + 51, renderDepth);
+		Renderer.drawString("default_med", dHit, x + 4, y + 66, renderDepth);
+		Renderer.drawString("default_med", dCrit, x + 4, y + 81, renderDepth);
+
 		// Bottom item and name
-		Renderer.drawRectangle(x, y+96, x + 90, y + 131, renderDepth,
+		Renderer.drawRectangle(x+1, y + 97, x + 89, y + 130, renderDepth,
 				defender.getPartyColor());
 		Renderer.drawString("default_med", defender.name, x + 2, y + 98,
 				renderDepth);
