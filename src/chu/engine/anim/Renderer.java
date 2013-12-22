72b3fdfa4de648afaef1feebb92d761e144619b2
chudooder <codeshawnachu@gmail.com>
2013-12-22 01:09:57 -0600

    Started shader framework

--- src/chu/engine/anim/Renderer.java
+++ src/chu/engine/anim/Renderer.java
@@ -2,6 +2,13 @@ package chu.engine.anim;
 
 import static org.lwjgl.opengl.GL11.*;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+import org.lwjgl.opengl.ARBFragmentShader;
+import org.lwjgl.opengl.ARBVertexShader;
+import org.lwjgl.opengl.ARBShaderObjects;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.opengl.Texture;
 
@@ -14,11 +21,16 @@ public class Renderer {
 	private static RectClip clip;
 	private static final int SCALE_FILTER = GL_NEAREST;
 	private static Color color;
+	private static int program;
+	private static int vertexShader;
+	private static int fragmentShader;
 
 	static {
 		camera = new Camera(null, 0, 0);
 		clip = null;
 		color = Color.white;
+		program = ARBShaderObjects.glCreateProgramObjectARB();
+		linkProgram();
 	}
 
 	/***
@@ -236,8 +248,44 @@ public class Renderer {
 		glPopMatrix();
 	}
 	
+	public void useVertexShader(String shaderName) {
+		 try {
+			vertexShader = createShader("shaders/"+shaderName+".vert",ARBVertexShader.GL_VERTEX_SHADER_ARB);
+			if(vertexShader == 0) throw new Exception();
+			ARBShaderObjects.glAttachObjectARB(program, vertexShader);
+		} catch (Exception e) {
+			System.err.println("Unable to create vertex shader: "+shaderName);
+			e.printStackTrace();
+		}
+	}
 	
+	public void useFragmentShader(String shaderName) {
+		try {
+			fragmentShader = createShader("shaders/"+shaderName+".frag",ARBFragmentShader.GL_FRAGMENT_SHADER_ARB);
+			if(fragmentShader == 0) throw new Exception();
+			ARBShaderObjects.glAttachObjectARB(program, fragmentShader);
+			ARBShaderObjects.glLinkProgramARB(program);
+	        linkProgram();
+		} catch (Exception e) {
+			System.err.println("Unable to create fragment shader: "+shaderName);
+			e.printStackTrace();
+		}
+	}
 	
+	private static void linkProgram() {
+		ARBShaderObjects.glLinkProgramARB(program);
+        if (ARBShaderObjects.glGetObjectParameteriARB(program, ARBShaderObjects.GL_OBJECT_LINK_STATUS_ARB) == GL_FALSE) {
+            System.err.println(getLogInfo(program));
+            return;
+        }
+        
+        ARBShaderObjects.glValidateProgramARB(program);
+        if (ARBShaderObjects.glGetObjectParameteriARB(program, ARBShaderObjects.GL_OBJECT_VALIDATE_STATUS_ARB) == GL_FALSE) {
+        	System.err.println(getLogInfo(program));
+        	return;
+        }
+	}
+
 	public static void setCamera(Camera c) {
 		camera = c;
 	}
@@ -252,6 +300,102 @@ public class Renderer {
     
     public static void removeClip() {
     	if(clip != null) clip.destroy();
+    }
+    
+    /*
+    * With the exception of syntax, setting up vertex and fragment shaders
+    * is the same.
+    * @param the name and path to the vertex shader
+    */
+    private int createShader(String filename, int shaderType) throws Exception {
+    	int shader = 0;
+    	try {
+	        shader = ARBShaderObjects.glCreateShaderObjectARB(shaderType);
+	        
+	        if(shader == 0)
+	        	return 0;
+	        
+	        ARBShaderObjects.glShaderSourceARB(shader, readFileAsString(filename));
+	        ARBShaderObjects.glCompileShaderARB(shader);
+	        
+	        if (ARBShaderObjects.glGetObjectParameteriARB(shader, ARBShaderObjects.GL_OBJECT_COMPILE_STATUS_ARB) == GL_FALSE)
+	            throw new RuntimeException("Error creating shader: " + getLogInfo(shader));
+	        
+	        return shader;
+    	}
+    	catch(Exception exc) {
+    		ARBShaderObjects.glDeleteObjectARB(shader);
+    		throw exc;
+    	}
+    }
+    
+    private static String getLogInfo(int obj) {
+        return ARBShaderObjects.glGetInfoLogARB(obj, 
+        		ARBShaderObjects.glGetObjectParameteriARB(obj,
+        				ARBShaderObjects.GL_OBJECT_INFO_LOG_LENGTH_ARB));
+    }
+    
+    /**
+     * I copied this method from the lwjgl website but this code makes me want
+     * to kill myself. I might fix later
+     * @param filename
+     * @return
+     * @throws Exception
+     */
+    private String readFileAsString(String filename) throws Exception {
+        StringBuilder source = new StringBuilder();
+        
+        FileInputStream in = new FileInputStream(filename);
+        
+        Exception exception = null;
+        
+        BufferedReader reader;
+        try{
+            reader = new BufferedReader(new InputStreamReader(in,"UTF-8"));
+            
+            Exception innerExc= null;
+            try {
+            	String line;
+                while((line = reader.readLine()) != null)
+                    source.append(line).append('\n');
+            }
+            catch(Exception exc) {
+            	exception = exc;
+            }
+            finally {
+            	try {
+            		reader.close();
+            	}
+            	catch(Exception exc) {
+            		if(innerExc == null)
+            			innerExc = exc;
+            		else
+            			exc.printStackTrace();
+            	}
+            }
+            
+            if(innerExc != null)
+            	throw innerExc;
+        }
+        catch(Exception exc) {
+        	exception = exc;
+        }
+        finally {
+        	try {
+        		in.close();
+        	}
+        	catch(Exception exc) {
+        		if(exception == null)
+        			exception = exc;
+        		else
+					exc.printStackTrace();
+        	}
+        	
+        	if(exception != null)
+        		throw exception;
+        }
+        
+        return source.toString();
     }
     
     static class RectClip {
