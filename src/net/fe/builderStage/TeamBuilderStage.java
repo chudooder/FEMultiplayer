cb9727b577cc8adf694090a2b93472d343c26950
Young Wang <jedyobidan@gmail.com>
2014-01-07 15:49:05 -0600

    Update to stage

--- src/net/fe/builderStage/TeamBuilderStage.java
+++ src/net/fe/builderStage/TeamBuilderStage.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import net.fe.unit.Unit;
 import net.fe.unit.UnitFactory;
+import chu.engine.Entity;
 import chu.engine.Stage;
 import chu.engine.anim.Renderer;
 
@@ -19,22 +20,31 @@ public class TeamBuilderStage extends Stage {
 	
 	@Override
 	public void render() {
-		int name = 0, clazz = 70, lv = 120, hp = 135;
+		int name = 0, clazz = 70, lv = 140, hgap = 30; //xvals
+		int yStart = 20, vgap = 20;
 		List<String> stats = Arrays.asList(
-			"Lvl", "HP", "Str", "Mag", "Spd", "Skl"
+			"Lvl", "HP", "Str", "Mag", "Skl", "Spd", "Lck", "Def", "Res", "Mov"
 		);
 		
 		Renderer.drawString("default_med", "Name", name, 0, 0);
 		Renderer.drawString("default_med", "Class", clazz, 0, 0);
-		Renderer.drawString("default_med", "Lv", lv, 0, 0);
-		Renderer.drawString("default_med", "HP", hp, 0, 0);
-//		Renderer.drawString("default_med", "Name", name, 0, 0);
-//		Renderer.drawString("default_med", "Name", name, 0, 0);
-//		for(int i=0; i<units.size(); i++) {
-//			Unit unit = units.get(i);
-//			Renderer.drawString("default_med", unit.name, 0, 16*i, 0.0f);
-//			Renderer.drawString("default_med", unit.getTheClass().name, 100, 16*i, 0.0f);
-//		}
+		int x = lv;
+		for(String s: stats){
+			Renderer.drawString("default_med", s, x, 0, 0);
+			x+= hgap;
+		}
+		
+		int y = yStart;
+		for(Unit u: units){
+			Renderer.drawString("default_med", u.name, name, y, 0);
+			Renderer.drawString("default_med", u.getTheClass().name, clazz, y, 0);
+			x = lv;
+			for(String s: stats){
+				Renderer.drawString("default_med", u.getBase(s), x, y, 0);
+				x+= hgap;
+			}
+			y+=vgap;
+		}
 	}
 
 	@Override
@@ -55,4 +65,13 @@ public class TeamBuilderStage extends Stage {
 		
 	}
 
+}
+
+class Cursor extends Entity{
+	int index;
+	public Cursor(float x, float y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+	
 }
