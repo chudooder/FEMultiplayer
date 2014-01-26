379ccf618a7c181a6dbd1224310d682ba0760462
jedyobidan <jedyobidan@gmail.com>
2014-01-25 21:28:21 -0600

    Auto generated json

---
+++ resourceGenerator.rb
@@ -0,0 +1,9 @@
+File.open("temp.json", "w") { |io| 
+  io.write("\t\t//AUTO-GENERATED JSON\n")
+  Dir.glob("res/map_mugshots/*.png") { |file|  
+    io.write("\t\t{\n")
+    io.write("\t\t\t\"name\":\t\t\"#{File.basename(file, ".*")}\",\n")
+    io.write("\t\t\t\"path\":\t\t\"#{file}\"\n")
+    io.write("\t\t},\n")
+  }
+}
