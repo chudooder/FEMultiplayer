189e8ef16733d348f1e94b33ef530da4365c69f0
Shawn <codeshawnachu@gmail.com>
2014-02-08 16:37:21 -0600

    Made winner message less crappy

--- src/net/fe/transition/OverworldEndTransition.java
+++ src/net/fe/transition/OverworldEndTransition.java
@@ -1,9 +1,13 @@
 package net.fe.transition;
 
+import org.newdawn.slick.Color;
+
+import net.fe.FEResources;
 import net.fe.Transition;
 import net.fe.overworldStage.EndGameStage;
 import chu.engine.Game;
 import chu.engine.anim.Renderer;
+import chu.engine.anim.Transform;
 
 public class OverworldEndTransition extends Transition{
 	
@@ -19,7 +23,13 @@ public class OverworldEndTransition extends Transition{
 	}
 	
 	public void render() {
-		Renderer.drawString("default_med", winner+" wins!", 250, 150, 0.1f);
+		String string = winner+" is the winner!";
+		int width = FEResources.getBitmapFont("default_med").getStringWidth(string);
+		Transform t = new Transform();
+		t.scaleX = 2;
+		t.scaleY = 2;
+		t.color = Color.green;
+		Renderer.drawString("default_med", string, 240-width, 130, 0.1f, t);
 		timer += Game.getDeltaSeconds();
 		if(timer > 3.0f) {
 			Renderer.translate(0,endPos);
