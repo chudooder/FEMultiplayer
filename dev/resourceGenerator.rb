9863603f09a81707d513b576c7070ba72d0b913c
jedyobidan <jedyobidan@gmail.com>
2014-02-10 18:54:51 -0600

    Auto generated resources

--- dev/resourceGenerator.rb
+++ dev/resourceGenerator.rb
@@ -1,6 +1,6 @@
 File.open("temp.json", "w") { |io| 
-  io.write("\t\t//AUTO-GENERATED JSON\n")
-  Dir.glob("res/map_anim/*.png") { |file|  
+  Dir.glob("res/palette/*.png") { |file|  
+    puts File.basename(file, ".*")
     io.write("\t\t{\n")
     io.write("\t\t\t\"name\":\t\t\"#{File.basename(file, ".*")}\",\n")
     io.write("\t\t\t\"path\":\t\t\"#{file}\"\n")
