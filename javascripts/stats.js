66954c0a31f45a647017e433b34b9a185e452e9e
chudooder <codeshawnachu@gmail.com>
2014-02-23 18:01:14 -0600

    Graphical tweaks to table

--- javascripts/stats.js
+++ javascripts/stats.js
@@ -1,5 +1,8 @@
 $(document).ready(function() 
     { 
-        $("#stats_table").tablesorter(); 
+		$('#stats_table').tablesorter({
+		   widgets: ['zebra'],
+		   widgetZebra: {css: ["odd","even"]} // css classes to apply to rows
+		});
     } 
 );
